40b9234ea8282963f5a215b13245a639fe7b8423
iChun <ohai.iChun@gmail.com>
2014-12-07 04:38:59 +0000

    Add cam size command.

--- src/main/java/us/ichun/mods/twitchplays/client/task/TaskCamera.java
+++ src/main/java/us/ichun/mods/twitchplays/client/task/TaskCamera.java
@@ -10,10 +10,13 @@ public class TaskCamera extends Task
     public int moveType; //UDLR;
 
     public int camDist;
+    public int camSize;
 
     public TaskCamera(WorldClient world, EntityPlayerSP player)
     {
         super(world, player);
+        camDist = -1;
+        camSize = -1;
     }
 
     @Override
@@ -49,9 +52,14 @@ public class TaskCamera extends Task
             TwitchPlays.tickHandlerClient.oriYaw = TwitchPlays.tickHandlerClient.camYaw;
             TwitchPlays.tickHandlerClient.oriPitch = TwitchPlays.tickHandlerClient.camPitch;
         }
-        else
+        else if(camDist != -1)
         {
             TwitchPlays.config.get("minicamDistance").set(camDist);
+            TwitchPlays.config.save();
+        }
+        else
+        {
+            TwitchPlays.config.get("minicamSize").set(camDist);
             TwitchPlays.config.save();
         }
     }
@@ -59,11 +67,11 @@ public class TaskCamera extends Task
     @Override
     public boolean requiresOp(String...args)
     {
-        return false;
+        return args.length == 3 && args[1].equals("size");
     }
 
     @Override
-    public boolean bypassOrder(String...args) { return args.length == 3 && (args[1].equals("dist") || args[1].equals("distance")); };
+    public boolean bypassOrder(String...args) { return args.length == 3 && (args[1].equals("dist") || args[1].equals("distance") || args[1].equals("size")); };
 
     @Override
     public boolean parse(String... args)
@@ -73,16 +81,31 @@ public class TaskCamera extends Task
             moveType = (args[1].equals("up") || args[1].equals("u")) ? 1 : (args[1].equals("down") || args[1].equals("d")) ? 2 : (args[1].equals("left") || args[1].equals("l")) ? 3 : (args[1].equals("right") || args[1].equals("r")) ? 4 : 0;
             return moveType != 0;
         }
-        else if(args.length == 3 && (args[1].equals("dist") || args[1].equals("distance")))
+        else if(args.length == 3)
         {
-            try
+            if((args[1].equals("dist") || args[1].equals("distance")))
             {
-                camDist = MathHelper.clamp_int(Integer.parseInt(args[2]), 5, 500);
-                return true;
+                try
+                {
+                    camDist = MathHelper.clamp_int(Integer.parseInt(args[2]), 5, 500);
+                    return true;
+                }
+                catch(NumberFormatException e)
+                {
+                    return false;
+                }
             }
-            catch(NumberFormatException e)
+            else if(args[1].equals("size"))
             {
-                return false;
+                try
+                {
+                    camSize = MathHelper.clamp_int(Integer.parseInt(args[2]), 5, 90);
+                    return true;
+                }
+                catch(NumberFormatException e)
+                {
+                    return false;
+                }
             }
         }
         return false;
